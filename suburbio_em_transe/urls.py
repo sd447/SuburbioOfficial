2556887909037f3d5c19e0b69b95347d640a1ca1
Lucas Souza <olucassouza446@gmail.com>
2024-06-01 01:44:34 -0300

    Resolvendo problemas de compatibilida dos estaticos com o django

--- suburbio_em_transe/urls.py
+++ suburbio_em_transe/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from app_suburbio import views
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
